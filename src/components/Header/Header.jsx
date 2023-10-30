5014b0aa8705b85ab9b618ef15325e1f8644c9f5
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2023-10-30 11:23:01 -0300

    feat(header): create header theme and profile button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,8 +13,28 @@ const Header = () => {
           <i className="ri-search-line"></i>
         </div>
 
-        <div className="header__toggle">
-          <i className="ri-menu-line"></i>
+        <div className="header__buttons">
+          <div className="theme__button">
+            <i class="ri-moon-line"></i>
+          </div>
+
+          <div className="header__profile">
+            <div className="header__profile-data">
+              <div className="header__profile-icon">
+                <i class="ri-user-3-line"></i>
+              </div>
+
+              <span className="header__profile-name">Lucas</span>
+            </div>
+
+            <div className="profile__arrow">
+              <i class="ri-arrow-down-s-line"></i>
+            </div>
+          </div>
+
+          <div className="header__toggle">
+            <i className="ri-menu-line"></i>
+          </div>
         </div>
       </div>
     </header>
