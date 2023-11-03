89650265e6d5c23a946e6d44b1f36be83edab474
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2023-11-03 10:22:06 -0300

    feat(layou): update dashboard layout

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,6 +1,11 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
+import Dashboard from "../pages/Dashboard";
+import City from "../pages/City";
+import State from "../pages/State";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
 
 const Routers = () => {
   return (
@@ -8,6 +13,11 @@ const Routers = () => {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/city" element={<City />} />
+        <Route path="/state" element={<State />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
